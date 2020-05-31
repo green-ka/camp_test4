f252891803bef8e79becfb96d5cebc29c50b2211
green-ka <mi.tom.0319@gmail.com>
2020-05-31 23:02:08 +0900

    cssの反映

--- login.php
+++ login.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link href="css/reset.css" rel="stylesheet">
-    <link href="css/index.css" rel="stylesheet">
+    <link href="css/login.css" rel="stylesheet">
     <link href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" rel="stylesheet">
 </head>
 <body>
